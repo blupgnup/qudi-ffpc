acd25d13bc64868d3127d9dfebcdccc28677f54b
PMA <pma@home>
2022-06-25 20:08:51 +0200

    Updated rhodescharz dummy oscilloscope

--- src/qudi/hardware/oscilloscope/rhode_schwarz_dummy.py
+++ src/qudi/hardware/oscilloscope/rhode_schwarz_dummy.py
@@ -26,41 +26,6 @@ from qudi.core.configoption import ConfigOption
 from qudi.util.mutex import Mutex
 from qudi.interface.oscilloscope_interface import OscilloscopeInterface
 
-class HardwarePull(QtCore.QObject):
-    """ Helper class for running the hardware communication in a separate thread. """
-
-    # signal to deliver the wavelength to the parent class
-    sig_trace = QtCore.Signal(list)
-
-    def __init__(self, parentclass):
-        super().__init__()
-
-        # remember the reference to the parent class to access functions ad settings
-        self._parentclass = parentclass
-
-    def handle_timer(self, state_change, channel):
-        """ Threaded method that can be called by a signal from outside to start the timer.
-        @param bool state: (True) starts timer, (False) stops it.
-        """
-        self.channel = channel
-        if state_change:
-            self.timer = QtCore.QTimer()
-            self.timer.timeout.connect(self._measure_thread)
-            self.timer.start(self._parentclass._measurement_timing)
-        else:
-            if hasattr(self, 'timer'):
-                self.timer.stop()
-
-    def _measure_thread(self):
-        """ The threaded method querying the data from the scope.
-        """
-        # update as long as the state is busy
-        if self._parentclass.module_state() == 'running':
-            trace = self._parentclass._rte.query_binary_values('FORM REAL,32;:CHAN{}:DATA?'.format(self.channel),
-                                                               datatype='f', is_big_endian=True)
-            # send the data to the parent via a signal
-            self.sig_trace.emit(trace)
-
 
 class OscilloscopeRS(OscilloscopeInterface):
     _address = ConfigOption('address', missing='error')
@@ -73,9 +38,6 @@ class OscilloscopeRS(OscilloscopeInterface):
 
     def __init__(self, config, **kwargs):
         super().__init__(config=config, **kwargs)
-
-        #locking for thread safety
-        self.threadlock = Mutex()
         
         self._current_trace = []
 
@@ -98,15 +60,25 @@ class OscilloscopeRS(OscilloscopeInterface):
         return self._current_trace
 
     def getData_cont(self, channel):
-        trace = self._rte.query_binary_values('FORM REAL,32;:CHAN{}:DATA?'.format(channel),
-                                              datatype='f', is_big_endian=True)
+        # trace = self._rte.query_binary_values('FORM REAL,32;:CHAN{}:DATA?'.format(channel),
+        #                                       datatype='f', is_big_endian=True)
+        """ Record a dummy trace.
+
+            @return ndarray: 1024-value ndarray containing random trace
+        """
+        recordlength = 1024
+
+        trace = np.empty((2, recordlength), dtype=np.double)
+        trace[0] = np.arange(730, 750, 20 / recordlength)
+        trace[1] = np.random.uniform(0, 2000, recordlength)
+
         self._current_trace = trace
         return self._current_trace
 
     def RunContinous(self, channel=1, refreshrate=None):
         """ SimpleDataInterface function to get the power from the scope """
-        self._rte.write('RUNContinous')
-        self._rte.query('*OPC?')
+        # self._rte.write('RUNContinous')
+        # self._rte.query('*OPC?')
         '''
         if refreshrate is not None:
             self._measurement_timing = refreshrate
@@ -119,19 +91,32 @@ class OscilloscopeRS(OscilloscopeInterface):
         return 0
 
     def SetVerticalScale(self, channel=1, scale=10e-3):
-        self._rte.write('CHAN{0}:SCAL {1}'.format(channel, scale))
+        # self._rte.write('CHAN{0}:SCAL {1}'.format(channel, scale))
+        pass
 
     def SetTimeBase(self, timebase=5e-3):
-        self._rte.write('TIMebase:RANGe {}'.format(timebase))
+        # self._rte.write('TIMebase:RANGe {}'.format(timebase))
+        pass
 
     def SetRecordLength(self, recordlength=1000):
-        self._rte.write('ACQ:POIN {}'.format(recordlength))
-        sleep(0.1)
+        # self._rte.write('ACQ:POIN {}'.format(recordlength))
+        # sleep(0.1)
+        pass
 
     def RunSingle(self, channel=1):
         #self._rte.write('RUNSingle')
-        trace = self._rte.query_binary_values('FORM REAL,32;:CHAN{}:DATA?'.format(channel),
-                                              datatype='f', is_big_endian=True)
+        # trace = self._rte.query_binary_values('FORM REAL,32;:CHAN{}:DATA?'.format(channel),
+        #                                       datatype='f', is_big_endian=True)
+        """ Record a dummy trace.
+
+            @return ndarray: 1024-value ndarray containing random trace
+        """
+        length = 1024
+
+        trace = np.empty((2, length), dtype=np.double)
+        trace[0] = np.arange(730, 750, 20 / length)
+        trace[1] = np.random.uniform(0, 2000, length)
+
         return trace    
 
     def RunSTOP(self):
@@ -144,9 +129,11 @@ class OscilloscopeRS(OscilloscopeInterface):
         return 0
 
     def get_xaxis(self):
-        timebase = float(self._rte.query('TIMebase:RANGe?'))
-        self._rte.query('*OPC?')
-        recordlength = int(self._rte.query('ACQ:POIN?'))
-        self._rte.query('*OPC?')
+        # timebase = float(self._rte.query('TIMebase:RANGe?'))
+        # self._rte.query('*OPC?')
+        # recordlength = int(self._rte.query('ACQ:POIN?'))
+        # self._rte.query('*OPC?')
+        timebase = 20e-6
+        recordlength = 1024
         xaxis = np.linspace(-timebase/2, timebase/2, recordlength)
         return xaxis
